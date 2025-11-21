b782d9ff60f04db0f79c3dfc7af6db9181f0be79
reharehavycode261 <stagiaire2.vycode@gmail.com>
2025-11-21 14:44:01 +0300

    Implémentation automatique - feature/5f530ea9-05aa88-1442

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,33 @@ import mg.uniDao.exception.DaoException;
 import mg.uniDao.provider.GenericSqlProvider;
 import project.Region;
 
+/**
+ * Classe principale pour initier la connexion à la base de données et effectuer des opérations CRUD de base.
+ * 
+ * Exemple d'utilisation:
+ * <pre>
+ * {@code
+ * public class Main {
+ *     public static void main(String[] args) throws DaoException {
+ *         // Créer une connexion à la base de données
+ *         Database postgresSql = GenericSqlProvider.get("database.json");
+ *
+ *         // Connecter au service de la base de données
+ *         Service service = postgresSql.connect("TEST", true);
+ *
+ *         // Créer une instance de Region
+ *         Region region = new Region("R002", "South Region");
+ *
+ *         // Mettre à jour une région dans la base de données par ID
+ *         postgresSql.updateById(service, region, 48);
+ *         
+ *         // Déconnecter le service
+ *         service.close();
+ *     }
+ * }
+ * }
+ * </pre>
+ */
 public class Main {
     public static void main(String[] args) throws DaoException {
         Database postgresSql = GenericSqlProvider.get("database.json");
@@ -13,6 +40,6 @@ public class Main {
         Region region = new Region();
         postgresSql.updateById(service, region, 48);
 
-        service.endConnection();
+        service.close();
     }
 }
--- src/Region.java
+++ src/Region.java
@@ -3,6 +3,24 @@ import mg.uniDao.annotation.Collection;
 import mg.uniDao.annotation.Field;
 import mg.uniDao.core.sql.GenericSqlDao;
 
+/**
+ * Représente une région avec un identifiant unique et une description.
+ * 
+ * Exemple d'utilisation:
+ * <pre>
+ * {@code
+ * // Créer une nouvelle instance de Region
+ * Region region = new Region();
+ * 
+ * // Initialiser une région avec des paramètres
+ * Region regionWithDetails = new Region("R001", "North Region");
+ * 
+ * // Afficher les informations de la région
+ * System.out.println(regionWithDetails.getRegionId());
+ * System.out.println(regionWithDetails.getRegionDescription());
+ * }
+ * </pre>
+ */
 @Collection
 public class Region extends GenericSqlDao {
     @AutoSequence(name = "student", prefix = "ETU", length = 8)
@@ -15,7 +33,24 @@ public class Region extends GenericSqlDao {
     public Region() {
     }
 
-    public Region(String regionDescription) {
+    public Region(String regionId, String regionDescription) {
+        this.regionId = regionId;
+        this.regionDescription = regionDescription;
+    }
+
+    public String getRegionId() {
+        return regionId;
+    }
+
+    public void setRegionId(String regionId) {
+        this.regionId = regionId;
+    }
+
+    public String getRegionDescription() {
+        return regionDescription;
+    }
+
+    public void setRegionDescription(String regionDescription) {
         this.regionDescription = regionDescription;
     }
 }
